fc102ef7ba7fa44a2d50812157870607dec1116d
i-zakiro <zakiro24x7@gmail.com>
2022-08-02 16:46:32 +0500

    45:08

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem } from "../../redux/slices/cartSlice";
 
-function PizzaBlock({ title, price, imageUrl, sizes, types }) {
+function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
+    const dispatch = useDispatch();
+    const cartItem = useSelector(state => state.cart.items.find(obj => obj.id == id));
     const [activeType, setActiveType] = React.useState(0);
     const [activeSize, setActiveSize] = React.useState(0);
     const typeNames = ['тонкое', 'традиционное'];
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    const addedCount = cartItem ? cartItem.count : 0;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    const onClickAdd = () => {
+      const item = {
+        id,
+        title,
+        price,
+        imageUrl,
+        type: activeType,
+        size: activeSize,
+      };
+
+      dispatch(addItem(item));
+    }
     
 
     return (
@@ -29,7 +52,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
         </div>
         <div className="pizza-block__bottom">
           <div className="pizza-block__price">от {price} ₽</div>
-          <button className="button button--outline button--add">
+          <button onClick={onClickAdd} className="button button--outline button--add">
             <svg
               width="12"
               height="12"
@@ -42,7 +65,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
               />
             </svg>
             <span>Добавить</span>
-            <i>0</i>
+            {addedCount > 0 && <i>{addedCount}</i>}
           </button>
         </div>
       </div>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -9,15 +9,39 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    // addItem  (state, action) {
+    //   state.items.push(action.payload);
+    //   state.totalPrice = state.items.reduce((sum, obj) => {
+    //     return obj.price + sum;
+    //   }, 0);
+    // },
+    // let users = [
+    //   {id: 1, name: "Вася"},
+    //   {id: 2, name: "Петя"},
+    //   {id: 3, name: "Маша"}
+    // ];
     addItem  (state, action) {
-      state.items.push(action.payload);
+      const findItem = state.items.find(obj => obj.id == action.payload.id);
+
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
     },
-    removeItem(state, action) {
-      state.items = state.items.filter(item => item.id !== action.payload);
-    },
-    clearItems(state) {
-      state.items = [];
-    },
+    // removeItem(state, action) {
+    //   state.items = state.items.filter(item => item.id !== action.payload);
+    // },
+    // clearItems(state) {
+    //   state.items = [];
+    // },
   },
 })
 
