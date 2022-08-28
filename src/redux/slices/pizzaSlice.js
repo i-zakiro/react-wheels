1dee59d4d391b49f2980a2df05e796bc7a98e830
ildar Zakirov <zakiro24x7@gmail.com>
2022-08-28 17:11:06 +0500

    done #17 start18

--- src/redux/slices/pizzaSlice.js
+++ src/redux/slices/pizzaSlice.js
@@ -13,7 +13,7 @@ export const fetchPizzas = createAsyncThunk(
     } = params;
 
     const { data } = await axios.get(
-        `https://62a43b1747e6e400638e8143.mockapi.io/items?page=${currentPage}&limit=4&${category}sortBy=${sortBy}&order=${order}${search}`,
+        `https://-62a43b1747e6e400638e8143.mockapi.io/items?page=${currentPage}&limit=4&${category}sortBy=${sortBy}&order=${order}${search}`,
       )
     return data;
   }
@@ -34,13 +34,13 @@ const pizzaSlice = createSlice({
     },
   },
   extraReducers: {
-    [fetchPizzas.pending]: (state) => {
+    'pizza/fetchPizzasStatus/pending': (state) => {
       state.status = 'loading';
       state.items = [];
       state.error = null;
       console.log('loaddddiing');
     },
-    [fetchPizzas.fulfilled]: (state, action) => {
+    'pizza/fetchPizzasStatus/fulfilled': (state, action) => {
       state.items = action.payload;
       state.status = 'success';
       console.log('succcccess');
